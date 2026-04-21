d4dbe179f1b486659e3eb22f262b5e10c6bd2b25
jt <jaterli@hotmail.com>
2026-04-21 18:33:37 +0200

    updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://jaterli.github.io/react-entrega-ejercicios-1'
+  base: 'https://jaterli.github.io/react-entrega-ejercicios-1/'
 })
